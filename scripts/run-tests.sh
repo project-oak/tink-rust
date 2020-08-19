8f0d2c973471b2d7de012a4b37a11b426decc12c
David Drysdale <drysdale@google.com>
2020-08-19 18:59:42 +0100

    Add testing helper script
    Assumes the existence of an upstream repo at TINK_DIR which has
    been patched to expect a Rust testing server binary to be included
    in the mix, using something equivalent to the patch files under
    scripts/*.patch.
    
    These patches are against upstream Tink commit 29539107b1de
    ("Fixes test failures that would result from `std::string_view(nullptr)`
    becoming undefined behavior").
    
    Change-Id: Ic8c1077cc12458d5580466a7362317578fabc368

---
+++ scripts/run-tests.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Copyright 2020 The Tink-Rust Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+readonly SCRIPTS_DIR="$(dirname "$0")"
+
+# Location of upstream Tink repo.
+readonly TINK_DIR=${TINK_DIR:-${SCRIPTS_DIR}/../../tink}
+
+cargo build --package=tink-testing-server
+
+cd ${TINK_DIR}/testing/cross_language
+(
+    cd ../cc
+    bazel build :testing_server
+    cd ../go
+    bazel build :testing_server
+    cd ../java_src
+    bazel build :testing_server :testing_server_deploy.jar
+    cd ../python
+    bazel build :testing_server
+)
+
+bazel test --cache_test_results=no \
+      :deterministic_aead_test \
+      :key_generation_consistency_test \
+      :json_test \
+      :mac_test \
+      :prf_set_test \
+      --test_env testing_dir=${PWD}/.. \
+      $@
