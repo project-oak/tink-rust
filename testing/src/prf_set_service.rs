bc5f4550b8aa037eb79178115bf9d832f3e3bc15
David Drysdale <drysdale@google.com>
2020-11-18 19:17:46 +0000

    tink-testing-server: reduce boilerplate

--- testing/src/prf_set_service.rs
+++ testing/src/prf_set_service.rs
@@ -28,25 +28,25 @@ impl proto::prf_set_server::PrfSet for PrfSetServerImpl {
         request: tonic::Request<proto::PrfSetKeyIdsRequest>,
     ) -> Result<tonic::Response<proto::PrfSetKeyIdsResponse>, tonic::Status> {
         let req = request.into_inner(); // discard metadata
-        let cursor = std::io::Cursor::new(req.keyset);
-        let mut reader = tink::keyset::BinaryReader::new(cursor);
-        let handle = match tink::keyset::insecure::read(&mut reader) {
-            Err(e) => return key_ids_rsp_from_err(e),
-            Ok(v) => v,
+        let closure = move || -> Result<_, tink::TinkError> {
+            let cursor = std::io::Cursor::new(req.keyset);
+            let mut reader = tink::keyset::BinaryReader::new(cursor);
+            let handle = tink::keyset::insecure::read(&mut reader)?;
+            let primitive = tink_prf::Set::new(&handle)?;
+            let mut output = proto::prf_set_key_ids_response::Output {
+                primary_key_id: primitive.primary_id,
+                key_id: Vec::new(),
+            };
+            for key_id in primitive.prfs.keys() {
+                output.key_id.push(*key_id);
+            }
+            Ok(output)
         };
-        let primitive = match tink_prf::Set::new(&handle) {
-            Err(e) => return key_ids_rsp_from_err(e),
-            Ok(v) => v,
-        };
-        let mut output = proto::prf_set_key_ids_response::Output {
-            primary_key_id: primitive.primary_id,
-            key_id: Vec::new(),
-        };
-        for key_id in primitive.prfs.keys() {
-            output.key_id.push(*key_id);
-        }
         Ok(tonic::Response::new(proto::PrfSetKeyIdsResponse {
-            result: Some(proto::prf_set_key_ids_response::Result::Output(output)),
+            result: Some(match closure() {
+                Ok(output) => proto::prf_set_key_ids_response::Result::Output(output),
+                Err(e) => proto::prf_set_key_ids_response::Result::Err(format!("{:?}", e)),
+            }),
         }))
     }
     async fn compute(
@@ -54,50 +54,18 @@ impl proto::prf_set_server::PrfSet for PrfSetServerImpl {
         request: tonic::Request<proto::PrfSetComputeRequest>,
     ) -> Result<tonic::Response<proto::PrfSetComputeResponse>, tonic::Status> {
         let req = request.into_inner(); // discard metadata
-        let cursor = std::io::Cursor::new(req.keyset);
-        let mut reader = tink::keyset::BinaryReader::new(cursor);
-        let handle = match tink::keyset::insecure::read(&mut reader) {
-            Err(e) => return compute_rsp_from_err(e),
-            Ok(v) => v,
-        };
-        let primitive = match tink_prf::Set::new(&handle) {
-            Err(e) => return compute_rsp_from_err(e),
-            Ok(v) => v,
-        };
-        let output = match primitive.prfs[&req.key_id]
-            .compute_prf(&req.input_data, req.output_length as usize)
-        {
-            Err(e) => return compute_rsp_from_err(e),
-            Ok(v) => v,
+        let closure = move || {
+            let cursor = std::io::Cursor::new(req.keyset.clone());
+            let mut reader = tink::keyset::BinaryReader::new(cursor);
+            let handle = tink::keyset::insecure::read(&mut reader)?;
+            let primitive = tink_prf::Set::new(&handle)?;
+            primitive.prfs[&req.key_id].compute_prf(&req.input_data, req.output_length as usize)
         };
         Ok(tonic::Response::new(proto::PrfSetComputeResponse {
-            result: Some(proto::prf_set_compute_response::Result::Output(output)),
+            result: Some(match closure() {
+                Ok(output) => proto::prf_set_compute_response::Result::Output(output),
+                Err(e) => proto::prf_set_compute_response::Result::Err(format!("{:?}", e)),
+            }),
         }))
     }
-}
-
-// The testing infrastructure expects errors to be included in the response,
-// rather than using the gRPC error reporting mechanism.  Include helpers to
-// make it easy to map `TinkError` instances to this.
-
-fn key_ids_rsp_from_err(
-    e: tink::TinkError,
-) -> Result<tonic::Response<proto::PrfSetKeyIdsResponse>, tonic::Status> {
-    Ok(tonic::Response::new(proto::PrfSetKeyIdsResponse {
-        result: Some(proto::prf_set_key_ids_response::Result::Err(format!(
-            "{:?}",
-            e
-        ))),
-    }))
-}
-
-fn compute_rsp_from_err(
-    e: tink::TinkError,
-) -> Result<tonic::Response<proto::PrfSetComputeResponse>, tonic::Status> {
-    Ok(tonic::Response::new(proto::PrfSetComputeResponse {
-        result: Some(proto::prf_set_compute_response::Result::Err(format!(
-            "{:?}",
-            e
-        ))),
-    }))
 }
--- testing/src/signature_service.rs
+++ testing/src/signature_service.rs
@@ -28,22 +28,18 @@ impl proto::signature_server::Signature for SignatureServerImpl {
         request: tonic::Request<proto::SignatureSignRequest>,
     ) -> Result<tonic::Response<proto::SignatureSignResponse>, tonic::Status> {
         let req = request.into_inner(); // discard metadata
-        let cursor = std::io::Cursor::new(req.private_keyset);
-        let mut reader = tink::keyset::BinaryReader::new(cursor);
-        let handle = match tink::keyset::insecure::read(&mut reader) {
-            Err(e) => return sign_rsp_from_err(e),
-            Ok(v) => v,
-        };
-        let signer = match tink_signature::new_signer(&handle) {
-            Err(e) => return sign_rsp_from_err(e),
-            Ok(v) => v,
-        };
-        let sig_value = match signer.sign(&req.data) {
-            Err(e) => return sign_rsp_from_err(e),
-            Ok(v) => v,
+        let closure = move || {
+            let cursor = std::io::Cursor::new(req.private_keyset.clone());
+            let mut reader = tink::keyset::BinaryReader::new(cursor);
+            let handle = tink::keyset::insecure::read(&mut reader)?;
+            let signer = tink_signature::new_signer(&handle)?;
+            signer.sign(&req.data)
         };
         Ok(tonic::Response::new(proto::SignatureSignResponse {
-            result: Some(proto::signature_sign_response::Result::Signature(sig_value)),
+            result: Some(match closure() {
+                Ok(sig) => proto::signature_sign_response::Result::Signature(sig),
+                Err(e) => proto::signature_sign_response::Result::Err(format!("{:?}", e)),
+            }),
         }))
     }
 
@@ -52,44 +48,18 @@ impl proto::signature_server::Signature for SignatureServerImpl {
         request: tonic::Request<proto::SignatureVerifyRequest>,
     ) -> Result<tonic::Response<proto::SignatureVerifyResponse>, tonic::Status> {
         let req = request.into_inner(); // discard metadata
-        let cursor = std::io::Cursor::new(req.public_keyset);
-        let mut reader = tink::keyset::BinaryReader::new(cursor);
-        let handle = match tink::keyset::insecure::read(&mut reader) {
-            Err(e) => return verify_rsp_from_err(e),
-            Ok(v) => v,
-        };
-        let verifier = match tink_signature::new_verifier(&handle) {
-            Err(e) => return verify_rsp_from_err(e),
-            Ok(v) => v,
-        };
-        match verifier.verify(&req.signature, &req.data) {
-            Err(e) => return verify_rsp_from_err(e),
-            Ok(()) => {}
+        let closure = move || {
+            let cursor = std::io::Cursor::new(req.public_keyset.clone());
+            let mut reader = tink::keyset::BinaryReader::new(cursor);
+            let handle = tink::keyset::insecure::read(&mut reader)?;
+            let verifier = tink_signature::new_verifier(&handle)?;
+            verifier.verify(&req.signature, &req.data)
         };
         Ok(tonic::Response::new(proto::SignatureVerifyResponse {
-            err: "".to_string(),
+            err: match closure() {
+                Ok(_) => "".to_string(),
+                Err(e) => format!("{:?}", e),
+            },
         }))
     }
-}
-
-// The testing infrastructure expects errors to be included in the response,
-// rather than using the gRPC error reporting mechanism.  Include helpers to
-// make it easy to map `TinkError` instances to this.
-
-fn sign_rsp_from_err(
-    e: tink::TinkError,
-) -> Result<tonic::Response<proto::SignatureSignResponse>, tonic::Status> {
-    Ok(tonic::Response::new(proto::SignatureSignResponse {
-        result: Some(proto::signature_sign_response::Result::Err(format!(
-            "{:?}",
-            e
-        ))),
-    }))
-}
-fn verify_rsp_from_err(
-    e: tink::TinkError,
-) -> Result<tonic::Response<proto::SignatureVerifyResponse>, tonic::Status> {
-    Ok(tonic::Response::new(proto::SignatureVerifyResponse {
-        err: format!("{:?}", e),
-    }))
 }
