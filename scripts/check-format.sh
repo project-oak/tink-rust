d20cc1a86c3850514fafa6af8be97d990eef0a73
David Drysdale <drysdale@google.com>
2020-11-19 13:30:37 +0000

    ci: add check for license header
    Also include code to check TODOs match an issue, but don't turn it
    on yet.

---
+++ scripts/check-format.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Find code files.
+CODE_FILES=()
+while IFS=  read -r -d $'\0'; do
+  CODE_FILES+=("$REPLY")
+done < <(find . -not \( -path '*/target' -prune \) -and -name '*.rs' -print0)
+
+# Find markdown files.
+MD_FILES=()
+while IFS=  read -r -d $'\0'; do
+  MD_FILES+=("$REPLY")
+done < <(find . -not \( -path '*/target' -prune \) -and -name '*.md' -print0)
+
+# Check that source files have the Apache License header.
+# Automatically skips generated files.
+check_license() {
+  local path="$1"
+
+  if head -1 "$path" | grep -iq -e 'generated' -e '::prost::message'; then
+    return 0
+  fi
+
+  if echo "$path" | grep -q "/proto/"; then
+    return 0
+  fi
+
+  # Look for "Apache License" on the file header
+  if ! head -10 "$path" | grep -q 'Apache License'; then
+    # Format: $path:$line:$message
+    echo "$path:1:license header not found"
+    return 1
+  fi
+  return 0
+}
+
+# Check that any TODO markers in files have associated issue numbers
+check_todo() {
+  local path="$1"
+  local result
+  result=$(grep --with-filename --line-number TODO "$path" | grep --invert-match --regexp='TODO(#[0-9][0-9]*)')
+  if [[ -n $result ]]; then
+    echo "$result"
+    return 1
+  fi
+  return 0
+}
+
+errcount=0
+for f in "${CODE_FILES[@]}"; do
+  check_license "$f"
+  errcount=$((errcount + $?))
+  # check_todo "$f"
+  # errcount=$((errcount + $?))
+done
+
+if [ $errcount -gt 0 ]; then
+  echo "$errcount errors detected"
+  exit 1
+fi
