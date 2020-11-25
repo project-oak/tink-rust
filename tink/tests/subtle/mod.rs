e978ef4e2505f11d41543f605ae59650d0053474
David Drysdale <drysdale@google.com>
2020-11-25 08:11:41 +0000

    tink::subtle: extra tests

--- tink/tests/subtle/mod.rs
+++ tink/tests/subtle/mod.rs
@@ -35,6 +35,10 @@ fn test_compute_hash() {
             want: "185f8db32271fe25f561a6fc938b2e264306ec304eda518007d1764826381969",
         },
         Case {
+            hf: HashType::Sha384,
+            want: "3519fe5ad2c596efe3e276a6f351b8fc0b03db861782490d45f7598ebd0ab5fd5520ed102f38c4a5ec834e98668035fc",
+        },
+        Case {
             hf: HashType::Sha512,
             want: "3615f80c9d293ed7402687f94b22d58e529b8cc7916f8fac7fddf7fbd5af4cf777d3d795a7a00a16bf7e7f3fb9561ee9baae480da9fe7a18769e71886b03f315",
         },
