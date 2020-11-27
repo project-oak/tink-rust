6e4ee5b82369b2208956504da965605ca2bff339
David Drysdale <drysdale@google.com>
2020-11-27 11:56:13 +0000

    tink: auto-generate serde annotations
    Instead of having a manual copy of the Keyset-related structs, instead
    configure prost-build to emit the equivalent annotations when auto-generating
    the code, into a separate serde-enabled copy of the generated code
    under codegen/serde/.
    
    This allows all of the keyset::copy parallel structures to be dropped
    (and with them some conversions too).

--- tink/src/keyset/json_io.rs
+++ tink/src/keyset/json_io.rs
@@ -37,20 +37,15 @@ impl<T: Read> super::Reader for JsonReader<T> {
     /// [`std::io::Read`].
     fn read(&mut self) -> Result<crate::proto::Keyset, TinkError> {
         let mut de = serde_json::Deserializer::from_reader(&mut self.r);
-        match copy::Keyset::deserialize(&mut de) {
-            Ok(cp) => Ok(cp.into()),
-            Err(e) => Err(wrap_err("failed to parse", e)),
-        }
+        crate::proto::Keyset::deserialize(&mut de).map_err(|e| wrap_err("failed to parse", e))
     }
 
     /// Return an [`EncryptedKeyset`](crate::proto::EncryptedKeyset) object from the underlying
     /// [`std::io::Read`].
     fn read_encrypted(&mut self) -> Result<crate::proto::EncryptedKeyset, TinkError> {
         let mut de = serde_json::Deserializer::from_reader(&mut self.r);
-        match copy::EncryptedKeyset::deserialize(&mut de) {
-            Ok(cp) => Ok(cp.into()),
-            Err(e) => Err(wrap_err("failed to parse", e)),
-        }
+        crate::proto::EncryptedKeyset::deserialize(&mut de)
+            .map_err(|e| wrap_err("failed to parse", e))
     }
 }
 
@@ -69,180 +64,14 @@ impl<T: Write> JsonWriter<T> {
 impl<T: Write> super::Writer for JsonWriter<T> {
     /// Write the keyset to the underlying [`std::io::Write`].
     fn write(&mut self, keyset: &crate::proto::Keyset) -> Result<(), TinkError> {
-        let keyset: copy::Keyset = keyset.clone().into();
-        serde_json::to_writer_pretty(&mut self.w, &keyset)
+        serde_json::to_writer_pretty(&mut self.w, keyset)
             .map_err(|e| wrap_err("failed to encode", e))
     }
 
     /// Write the encrypted keyset to the underlying [`std::io::Write`].
     fn write_encrypted(&mut self, keyset: &crate::proto::EncryptedKeyset) -> Result<(), TinkError> {
-        let keyset: copy::EncryptedKeyset = keyset.clone().into();
-        serde_json::to_writer_pretty(&mut self.w, &keyset)
+        serde_json::to_writer_pretty(&mut self.w, keyset)
             .map_err(|e| wrap_err("failed to encode", e))
-    }
-}
-
-mod copy {
-    //! Copies of key (pun intended) Prost-generated structures to allow serde traits to be defined
-    //! for them, together with `From` trait implementations to allow conversion back and forth.
-    // This is basically a manual implementation of serde's [`remote` feature](https://serde.rs/remote-derive.html).
-    // TODO: get auto-generation working.
-    use serde::{Deserialize, Serialize};
-
-    #[derive(Serialize, Deserialize)]
-    #[serde(rename_all = "camelCase")]
-    pub struct Keyset {
-        pub primary_key_id: u32,
-        pub key: Vec<Key>,
-    }
-    #[derive(Serialize, Deserialize)]
-    #[serde(rename_all = "camelCase")]
-    pub struct Key {
-        pub key_data: Option<KeyData>,
-        #[serde(with = "crate::keyset::key_status_type")]
-        pub status: i32,
-        pub key_id: u32,
-        #[serde(with = "crate::keyset::output_prefix_type")]
-        pub output_prefix_type: i32,
-    }
-    #[derive(Serialize, Deserialize)]
-    #[serde(rename_all = "camelCase")]
-    pub struct KeyData {
-        pub type_url: String,
-        #[serde(with = "crate::keyset::b64")]
-        pub value: Vec<u8>,
-        #[serde(with = "crate::keyset::key_material_type")]
-        pub key_material_type: i32, // key_data::KeyMaterialType enum
-    }
-    #[derive(Serialize, Deserialize)]
-    #[serde(rename_all = "camelCase")]
-    pub struct EncryptedKeyset {
-        #[serde(with = "crate::keyset::b64")]
-        pub encrypted_keyset: Vec<u8>,
-        pub keyset_info: Option<KeysetInfo>,
-    }
-    #[derive(Serialize, Deserialize)]
-    #[serde(rename_all = "camelCase")]
-    pub struct KeysetInfo {
-        pub primary_key_id: u32,
-        pub key_info: Vec<KeyInfo>,
-    }
-    #[derive(Serialize, Deserialize)]
-    #[serde(rename_all = "camelCase")]
-    pub struct KeyInfo {
-        pub type_url: String,
-        #[serde(with = "crate::keyset::key_status_type")]
-        pub status: i32,
-        pub key_id: u32,
-        #[serde(with = "crate::keyset::output_prefix_type")]
-        pub output_prefix_type: i32,
-    }
-
-    impl From<Keyset> for crate::proto::Keyset {
-        fn from(copy: Keyset) -> Self {
-            Self {
-                primary_key_id: copy.primary_key_id,
-                key: copy.key.into_iter().map(|k| k.into()).collect(),
-            }
-        }
-    }
-    impl From<crate::proto::Keyset> for Keyset {
-        fn from(proto: crate::proto::Keyset) -> Self {
-            Self {
-                primary_key_id: proto.primary_key_id,
-                key: proto.key.into_iter().map(|k| k.into()).collect(),
-            }
-        }
-    }
-    impl From<Key> for crate::proto::keyset::Key {
-        fn from(copy: Key) -> Self {
-            Self {
-                key_data: copy.key_data.map(|kd| kd.into()),
-                status: copy.status,
-                key_id: copy.key_id,
-                output_prefix_type: copy.output_prefix_type,
-            }
-        }
-    }
-    impl From<crate::proto::keyset::Key> for Key {
-        fn from(proto: crate::proto::keyset::Key) -> Self {
-            Self {
-                key_data: proto.key_data.map(|kd| kd.into()),
-                status: proto.status,
-                key_id: proto.key_id,
-                output_prefix_type: proto.output_prefix_type,
-            }
-        }
-    }
-    impl From<KeyData> for crate::proto::KeyData {
-        fn from(copy: KeyData) -> Self {
-            Self {
-                type_url: copy.type_url,
-                value: copy.value,
-                key_material_type: copy.key_material_type,
-            }
-        }
-    }
-    impl From<crate::proto::KeyData> for KeyData {
-        fn from(proto: crate::proto::KeyData) -> Self {
-            Self {
-                type_url: proto.type_url,
-                value: proto.value,
-                key_material_type: proto.key_material_type,
-            }
-        }
-    }
-    impl From<EncryptedKeyset> for crate::proto::EncryptedKeyset {
-        fn from(copy: EncryptedKeyset) -> Self {
-            Self {
-                encrypted_keyset: copy.encrypted_keyset,
-                keyset_info: copy.keyset_info.map(|ki| ki.into()),
-            }
-        }
-    }
-    impl From<crate::proto::EncryptedKeyset> for EncryptedKeyset {
-        fn from(proto: crate::proto::EncryptedKeyset) -> Self {
-            Self {
-                encrypted_keyset: proto.encrypted_keyset,
-                keyset_info: proto.keyset_info.map(|ki| ki.into()),
-            }
-        }
-    }
-    impl From<KeysetInfo> for crate::proto::KeysetInfo {
-        fn from(copy: KeysetInfo) -> Self {
-            Self {
-                primary_key_id: copy.primary_key_id,
-                key_info: copy.key_info.into_iter().map(|ki| ki.into()).collect(),
-            }
-        }
-    }
-    impl From<crate::proto::KeysetInfo> for KeysetInfo {
-        fn from(proto: crate::proto::KeysetInfo) -> Self {
-            Self {
-                primary_key_id: proto.primary_key_id,
-                key_info: proto.key_info.into_iter().map(|ki| ki.into()).collect(),
-            }
-        }
-    }
-    impl From<KeyInfo> for crate::proto::keyset_info::KeyInfo {
-        fn from(copy: KeyInfo) -> Self {
-            Self {
-                type_url: copy.type_url,
-                status: copy.status,
-                key_id: copy.key_id,
-                output_prefix_type: copy.output_prefix_type,
-            }
-        }
-    }
-    impl From<crate::proto::keyset_info::KeyInfo> for KeyInfo {
-        fn from(proto: crate::proto::keyset_info::KeyInfo) -> Self {
-            Self {
-                type_url: proto.type_url,
-                status: proto.status,
-                key_id: proto.key_id,
-                output_prefix_type: proto.output_prefix_type,
-            }
-        }
     }
 }
 
