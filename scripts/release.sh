df0c307068f331c9710bd967bbc8ab6c32fe267b
David Drysdale <drysdale@google.com>
2020-12-23 08:35:49 +0000

    scripts: crate release helper

---
+++ scripts/release.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+# Crates to be published. Order is significant; later crates can only rely on earlier crates
+CRATE_DIRS=(proto tink prf mac aead daead streaming signature integration/awskms integration/gcpkms rinkey)
+
+# Release crates in dependency order. Assumes `cargo login` has been done.
+for dir in "${CRATE_DIRS[@]}"; do
+    cargo release --manifest-path "$dir/Cargo.toml"
+done
