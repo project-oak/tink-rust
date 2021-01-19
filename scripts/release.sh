09ea696ea2f0fec26ae79ba9e363b62d0a4c13f6
David Drysdale <drysdale@google.com>
2021-01-19 09:14:12 +0000

    scripts/release.sh: fix command (#79)

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,5 +6,5 @@ CRATE_DIRS=(proto tink prf mac aead daead streaming signature integration/awskms
 
 # Release crates in dependency order. Assumes `cargo login` has been done.
 for dir in "${CRATE_DIRS[@]}"; do
-    cargo release --manifest-path "$dir/Cargo.toml"
+    cargo publish --manifest-path "$dir/Cargo.toml"
 done
