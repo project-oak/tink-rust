a21e5f227f1fb1b14c01b3295e83403ae39ceea9
David Drysdale <drysdale@google.com>
2021-05-19 10:11:18 +0100

    scripts: separate out tag-crates.sh

--- scripts/rev-up.sh
+++ scripts/rev-up.sh
@@ -32,13 +32,3 @@ cargo clippy --all-targets
 git add Cargo.lock
 
 git commit -m "Update crate versions to $VERSION"
-git tag "v$VERSION"
-
-# Also add tags for all released crates (in case they move independently)
-RELEASED_CRATE_DIRS=(proto core prf mac aead daead streaming signature integration/awskms integration/gcpkms rinkey)
-for dir in "${RELEASED_CRATE_DIRS[@]}"; do
-    crate_name=$(crate_name "$dir")
-    crate_tag="${crate_name}-${VERSION}"
-    echo "Add tag $crate_tag"
-    git tag "$crate_tag"
-done
