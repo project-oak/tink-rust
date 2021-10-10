42387ce6072701d4acafce44884e4d07e2d23203
David Drysdale <drysdale@google.com>
2021-10-10 08:36:31 +0100

    tink-prf: add example program

---
+++ examples/prf/src/main.rs
@@ -0,0 +1,28 @@
+// Copyright 2020 The Tink-Rust Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+//! Example program demonstrating `tink-prf`
+
+fn main() {
+    tink_prf::init();
+    let kh = tink_core::keyset::Handle::new(&tink_prf::hmac_sha256_prf_key_template()).unwrap();
+    let m = tink_prf::Set::new(&kh).unwrap();
+
+    let pt = b"need pseudo-random data deterministically produced from this input";
+    let out = m.compute_primary_prf(pt, 16).unwrap();
+    println!("'{}' => {}", String::from_utf8_lossy(pt), hex::encode(&out));
+    assert_eq!(out.len(), 16);
+}
